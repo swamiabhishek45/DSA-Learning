8ab015ebf5f5f8938bcbc72c8dce8ecbaa3ab7f6
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-31 17:28:48 +0530

    change

--- practice.cpp
+++ practice.cpp
@@ -3,16 +3,6 @@ using namespace std;
 
 int main()
 {
-    int n = 0111100;
-    int cnt = 0;
-    while(n != 0){
-        if(n&1){
-            cnt++;
-        }
-
-        n = n >> 1;
-    }
-
-    cout << cnt;
+    
     return 0;
 }
